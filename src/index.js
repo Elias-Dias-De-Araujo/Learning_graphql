c6e83874144e75eb039730cd14c81d2f92ed80fe
Elias_Dias <eliasdiasdearaujo2@gmail.com>
2022-03-04 19:15:59 -0300

    feature: Create old_index
    The old_index is the file that i had create to learn the basic fundamentals of graphQL

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const { ApolloServer, gql } = require('apollo-server');
-
-const typedefs = gql``;
-const resolvers = {};
-
-const server  
-
---
+++ src/old_index.js
@@ -0,0 +1,86 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// Todo request é POST
+// Toda request chega no mesmo endpoint (/graphql)
+
+// Query => Obter informações, Paralelo com rest -> (GET)
+// Mutation => Manipular dados, Paralelo com rest  -> (POST, PUT, PATCH, DELETE)
+// Scalar Types => String, Int, Boolean, Float e ID
+
+const typeDefs = gql`
+    type User {
+        id: ID!,
+        name: String!,
+        email: String!,
+        active: Boolean!,
+    }
+
+    type Post {
+        id: ID!,
+        title: String!,
+        content: String!,
+        author: User!,
+    }
+
+    type Query {
+        hello: String, 
+        users: [User!]!,
+        getUserByEmail(email: String!): User!
+    }
+
+    type Mutation {
+        createUser(
+            name: String!,
+            email: String!,
+        ): User!
+    }
+`;
+
+const users = [
+    {
+        id: String(Math.random()),
+        name: 'Elias',
+        email: 'Elias@teste.com',
+        active: true
+    },
+    {
+        id: String(Math.random()),
+        name: 'Rafael',
+        email: 'Rafael@teste.com',
+        active: false
+    },
+    {
+        id: String(Math.random()),
+        name: 'Islanio',
+        email: 'Islanio@teste.com',
+        active: true
+    },
+]
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello World',
+        users: () => users,
+        getUserByEmail: (_, args) => {
+            return users.find((user) => user.email == args.email);
+        }
+    },
+    Mutation: {
+        createUser: (_, args) => {
+            const newUser = {
+                id: String(Math.random()),
+                name: args.name,
+                email: args.email,
+                active: true,
+            };
+
+            users.push(newUser);
+            return newUser;
+        }
+    }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log(`Server is runing ${url}`));
+
