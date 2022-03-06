d4ee86d907ecbc523e2287d9f1edd96ddc914f20
Elias_Dias <eliasdiasdearaujo2@gmail.com>
2022-03-06 16:45:03 -0300

    feature: config mongoose, and create models

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true 
+    },
+    lastName: {
+        type: String,
+        required: true 
+    },
+    email: {
+        type: String,
+        required: true 
+    },
+    active: {
+        type: Boolean,
+        required: true 
+    }
+});
+
+export default mongoose.model('User', schema);
--- src/start_server.js
+++ src/start_server.js
@@ -2,7 +2,10 @@ import { ApolloServer } from 'apollo-server'
 import mongoose from 'mongoose';
 
 const startServer = ({ typeDefs, resolvers }) => {
-    //mongoose.connect('mongodb://localhost:')
+    mongoose.connect('mongodb://localhost:27017/graphql', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 
     const server = new ApolloServer({ typeDefs, resolvers });
     server.listen().then(({ url }) => console.log(`Server is running at ${url} 🚀`));
